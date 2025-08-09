b5ed862bbca54cee658236d563a1c789ab488801
Jacob <jjh413@student.bham.ac.uk>
2025-08-09 10:45:44 +0100

    sort: insertion sort

--- src/sort.zig
+++ src/sort.zig
@@ -21,6 +21,30 @@ pub fn bubbleSort(comptime T: type, list: []T, comparatorFn: fn (lhs: T, rhs: T)
     }
 }
 
+/// Insertion sorts a list, has an upper bound O(n^2) time complexity, although
+/// this is good for small lists n < 12 or so.
+pub fn insertionSort(comptime T: type, list: []T, comparatorFn: fn (lhs: T, rhs: T) std.math.Order) void {
+    for (0..list.len - 1) |i| {
+        var insertAt = i + 1;
+        var needsSwap = false;
+
+        for (i + 1..list.len) |j| {
+            const left = list[i];
+            const right = list[j];
+
+            const order = comparatorFn(left, right);
+
+            if (order.compare(.gt)) {
+                insertAt = j;
+                needsSwap = true;
+                continue;
+            }
+        }
+
+        if (needsSwap) swapElems(T, list, i, insertAt);
+    }
+}
+
 /// Just a wrapper for sorting lists of u8 in ascending order.
 pub fn orderU8(lhs: u8, rhs: u8) std.math.Order {
     return std.math.order(lhs, rhs);
@@ -38,6 +62,15 @@ test "bubbleSortReverseOrderedList" {
     var toSort = [_]u8{ 21, 10, 9, 6, 3, 2, 1 };
 
     bubbleSort(u8, toSort[0..], orderU8);
+
+    try std.testing.expectEqualSlices(u8, &expected, &toSort);
+}
+
+test "insertionSortReverseOrderedList" {
+    const expected = [_]u8{ 1, 2, 3, 6, 9, 10, 21 };
+    var toSort = [_]u8{ 21, 10, 9, 6, 3, 2, 1 };
+
+    insertionSort(u8, toSort[0..], orderU8);
 
     try std.testing.expectEqualSlices(u8, &expected, &toSort);
 }
